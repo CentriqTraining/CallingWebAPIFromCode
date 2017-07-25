f843732beb38cc0dcc93c932fead11378544d3fb
Mike Rissen <mrissen@centriq.com>
2017-07-25 14:42:01 -0500

    Initial Commit

---
+++ CallingWebApiFromCode/CallingWebApiFromCode/Program.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+
+namespace CallingWebApiFromCode
+{
+    class Program
+    {
+        static   void Main(string[] args)
+        {
+            //  Need to have Newtonsoft.JSON library installed
+            //  Either find it from "Manage nuget packages" from right-clicking the project
+            //  Or pull up package manager console and type "Install-package newtonsoft.json"
+            string url = "http://centriqdata.azurewebsites.net/data/toons";
+
+            //  Using web client
+            WebClient client = new WebClient();
+            var results = client.DownloadString(new Uri(url));
+            var Toons = JsonConvert.DeserializeObject<IEnumerable<Toon>>(results);
+        }
+    }
+}
---
+++ CallingWebApiFromCode/CallingWebApiFromCode/Toon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingWebApiFromCode
+{
+    public class Toon
+    {
+        public string Studio { get; set; }
+        public string Show { get; set; }
+        public string Character { get; set; }
+
+    }
+}
